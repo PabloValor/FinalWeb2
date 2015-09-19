a9ae5f6d159826a210f2e5d162cc492fd1982203
Pablo Valor <pablovalor89@gmail.com>
2015-09-19 18:11:02 -0300

    Se agrega Angularjs

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
-<body>
-    <h1>home!</h1>
+<body ng-app>
+    <h1>home! {{1+1+1}}</h1>
 
     <script type="text/javascript" src="public/scripts/vendor/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="public/scripts/vendor/angular.min.js"></script>
     <script type="text/javascript" src="public/scripts/main.js"></script>
 </body>
 </html>
